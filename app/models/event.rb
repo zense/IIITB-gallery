5771067b3a35c55188c56e0f96fdcb1560f740c4
Satvik Ramaprasad <satviksr@gmail.com>
2018-05-17 11:58:19 +0530

    Caching
    Changes to HomePage

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :group
   has_many :photo_records , dependent: :destroy
+
+  after_commit :clearCache
+
+  def clearCache
+	  ActionController::Base.expire_page(Rails.application.routes.url_helpers.gallery_event_path id:self.id,event_name: self.name)
+	  ActionController::Base.expire_page(Rails.application.routes.url_helpers.gallery_path)
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module GalleryIiitb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+	config.action_controller.page_cache_directory = "#{Rails.root}/public/cached_pages"
   end
 end
